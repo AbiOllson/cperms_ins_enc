ca72667a2d6ea8d552a8ab185be1fcbfa3007a35
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2026-09-21 11:51:13 +0100

    pp returns children in same order as cells_to_place returns them

--- insertion_encoding/rgf_clouds/pointplacement.py
+++ insertion_encoding/rgf_clouds/pointplacement.py
@@ -17,62 +17,6 @@ from .rgf_tracked_tiling import TrackedTiling
 Cell = tuple[int, int]
 
 
-class TrackedPointPlacementRGF(TrackedPointPlacement):
-    """Point placement tracking clouds."""
-
-    def __init__(self, tracked_tiling: TrackedTiling) -> None:
-        self.tracked_tiling = tracked_tiling
-        super().__init__(tracked_tiling)
-
-    def tracked_point_placement(
-        self,
-        requirement_list: tuple[GriddedCayleyPerm, ...],
-        indices: tuple[int, ...],
-        direction: int,
-    ) -> tuple[TrackedTiling, ...]:
-        """Yield the tilings with tracked clouds after point placement."""
-        if direction not in self.DIRECTIONS:
-            raise ValueError(f"Direction {direction} is not a valid direction.")
-        cells = self.cells_to_place_in(requirement_list, indices)
-        all_tracked_tilings = []
-        for placed_cell in cells:
-            placed_row = placed_cell[1]
-
-            rgf_obs = set()
-            if placed_row not in self.tracked_tiling.rows_had_vals:
-                x, y = placed_cell
-                _, m = self.tracked_tiling.dimensions
-                for i in range(0, x + 1):
-                    for j in range(y + 1, m + 2):
-                        rgf_obs.add(GriddedCayleyPerm([0], [(i, j)]))
-            tiling = self.point_placement_in_cell(
-                requirement_list, indices, direction, placed_cell
-            ).add_obstructions(rgf_obs)
-            map_for_cells = self.multiplex_map(placed_cell)
-            indices_clouds, value_clouds = TrackedTiling.map_clouds(
-                indices_clouds=self.tracked_tiling.indices_clouds,
-                value_clouds=self.tracked_tiling.value_clouds,
-                tiling_map=map_for_cells,
-            )
-            row_map = map_for_cells.preimage_map()[1]
-            rows_had_vals = set(
-                row_map[row][0]
-                for row in self.tracked_tiling.rows_had_vals
-                if row != placed_row
-            )
-            rows_had_vals.add(placed_cell[1] + 1)
-            all_tracked_tilings.append(
-                TrackedTiling(
-                    tiling,
-                    indices_clouds=indices_clouds,
-                    value_clouds=value_clouds,
-                    intersect_clouds_with_active=True,
-                    rows_had_vals=rows_had_vals,
-                )
-            )
-        return tuple(all_tracked_tilings)
-
-
 class TrackedRGFVPointPlacement(RGFVPointPlacement):
     """Point placement for restricted growth functions
     vertical insertion encoding."""
@@ -90,14 +34,14 @@ class TrackedRGFVPointPlacement(RGFVPointPlacement):
         cells = set()
         for idx, gcp in zip(indices, requirement_list):
             cells.add(gcp.positions[idx])
-        return sorted(cells)
+        return cells
 
     def tracked_point_placement(
         self,
         requirement_list: tuple[GriddedCayleyPerm, ...],
         indices: tuple[int, ...],
         direction: int,
-    ) -> tuple[TrackedTiling, ...]:
+    ) -> tuple[TrackedTiling | None, ...]:
         """Point placement for restricted growth functions.
 
         If vertical and only one row then just do a new max placement.
@@ -110,24 +54,24 @@ class TrackedRGFVPointPlacement(RGFVPointPlacement):
         if direction not in self.DIRECTIONS:
             raise ValueError(f"Direction {direction} is not a valid direction.")
         cells_in_rows = defaultdict(set)
-        point_placements = []
-        for cell in self.cells_to_place_in(requirement_list, indices):
+        point_placements = {}
+        order_for_cells = self.cells_to_place_in(requirement_list, indices)
+        point_placements: dict[Cell, TrackedTiling | None] = {
+            cell: None for cell in order_for_cells
+        }
+        for cell in order_for_cells:
             cells_in_rows[cell[1]].add(cell)
         for row in cells_in_rows:
             cells = sorted(cells_in_rows[row])
             if row in self.tracked_tiling.rows_had_vals:
                 for cell in cells:
-                    point_placements.append(
-                        self.tracked_point_placement_in_cell(
-                            requirement_list, indices, direction, cell
-                        )
+                    point_placements[cell] = self.tracked_point_placement_in_cell(
+                        requirement_list, indices, direction, cell
                     )
             else:
                 leftmost_cell = min(cells)
-                point_placements.append(
-                    self.new_max_point_placement(
-                        requirement_list, indices, direction, leftmost_cell
-                    )
+                point_placements[leftmost_cell] = self.new_max_point_placement(
+                    requirement_list, indices, direction, leftmost_cell
                 )
                 for cell in cells:
                     if cell != leftmost_cell:
@@ -139,42 +83,10 @@ class TrackedRGFVPointPlacement(RGFVPointPlacement):
                             for j in range(y + 1, m + 2):
                                 rgf_obs.add(GriddedCayleyPerm([0], [(i, j)]))
 
-                        point_placements.append(
-                            self.tracked_point_placement_in_cell(
-                                requirement_list, indices, direction, cell
-                            ).add_obstructions(rgf_obs)
-                        )
-
-        return tuple(point_placements)
-        # top_row = self.tiling.dimensions[1] - 1
-        # if
-        # if top_row == 0:
-        #     return (
-        #         self.new_max_point_placement(
-        #             requirement_list,
-        #             indices,
-        #             direction,
-        #             min(self.tiling.active_cells),
-        #         ),
-        #     )
-        point_placements = []
-        top_leftmost_cell = min(self.tiling.cells_in_row(top_row))
-        point_placements.append(
-            self.new_max_point_placement(
-                requirement_list,
-                indices,
-                direction,
-                top_leftmost_cell,
-            )
-        )
-        for row in range(top_row):
-            point_placements.extend(
-                self.tracked_point_placement_in_cell(
-                    requirement_list, indices, direction, cell
-                )
-                for cell in sorted(self.tiling.cells_in_row(row))
-            )
-            return tuple(point_placements)
+                        point_placements[cell] = self.tracked_point_placement_in_cell(
+                            requirement_list, indices, direction, cell
+                        ).add_obstructions(rgf_obs)
+        return tuple(point_placements.values())
 
     def new_max_point_placement(
         self,
@@ -225,23 +137,11 @@ class TrackedRGFVPointPlacement(RGFVPointPlacement):
         )
 
 
-# class TrackedRequirementPlacementStrategyRGF(
-#     TrackedRequirementPlacementStrategy,
-# ):
-#     """
-#     A strategy for placing requirements with tracked clouds.
-#     """
-
-#     def algorithm(self, tiling):
-#         """Return the point placement algorithm to use for the strategy."""
-#         return TrackedPointPlacementRGF(tiling)
-
-
 class TrackedRGFVRequirementPlacementStrategy(TrackedRequirementPlacementStrategy):
     """Strategy for placing requirements in a tiling for restricted growth functions
     vertical insertion encoding."""
 
-    def algorithm(self, tiling: TrackedTiling) -> TrackedPointPlacementRGF:
+    def algorithm(self, tiling: TrackedTiling) -> TrackedRGFVPointPlacement:
         return TrackedRGFVPointPlacement(tiling)
 
     def formal_step(self):
